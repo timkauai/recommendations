173b65b1a82c881ba70732d9cfffb5d22d9810e9
james <jpratt20@ischool.org>
2020-08-19 13:57:48 -1000

    inital commit, completely empty

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>James' Recommendations</h1>
+      <p>
+        A blog for James's Recommendations. I write about everything, from books
+        to movies to video games. I basically will write about things that I am
+        enjoying in life! The plan is to add some cool features to it, like
+        search, comment and more!!
+      </p>
     </div>
   );
 }
